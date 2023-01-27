49e0cb0ffc2ebad666e9665f2c9238a7269f595c
Chukwudi <cchinwenmeri@gmail.com>
2023-01-27 03:10:37 +0100

    tailwind rework

---
+++ implementation/tailwind/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+        extend: {
+            colors: {
+                'sedan-color': 'hsl(31, 77%, 52%)',
+                'suv-color': 'hsl(184, 100%, 22%)',
+                'lux-color': 'hsl(179, 100%, 13%)',
+                'fontColor1': 'hsla(0, 0%, 100%, 0.75)',
+                'fontColor2': 'hsl(0, 0%, 95%)'
+            },
+            fontFamily: {
+                lexend: ['"Lexend Deca"', 'cursive'],
+                bigShoulder: ['"Big Shoulders Display"', 'sans-serif']
+
+            }
+        },
+    },
+    plugins: [],
+}
